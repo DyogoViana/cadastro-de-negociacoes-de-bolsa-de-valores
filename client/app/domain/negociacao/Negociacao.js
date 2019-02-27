1aa34eb85dfe33ed18297d1bbd95c7ecb852c4c8
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-02-27 01:01:53 -0300

    Objeto imutável; Object.freeze (); Object.assign(); -- pg:48

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -2,18 +2,18 @@
 
 class Negociacao {
 
-    constructor (data, quantidade, valor) {
+    constructor (_data, _quantidade, _valor) {
 
-        this._data = data; // Data atual.
-        this._quantidade = quantidade;
-        this._valor = valor;
+        Object.assign (this, { _quantidade, _valor });
+        this._data = new Date (data.getTime()),
+        Object.freeze (this); // Objeto imutável, não pode ser editado.
     }
 
     //  Métodos acessadores.
-    getData () { return this._data; }
-    getQuantidade () { return this._quantidade; }
-    getValor () { return this._valor; }
-    getVolume () { return this._quantidade * this._valor; }
+    get data () { return new Date (this._data.getTime ()); }
+    get quantidade () { return this._quantidade; }
+    get valor () { return this._valor; }
+    get volume () { return this._quantidade * this._valor; }
 }
 
 
