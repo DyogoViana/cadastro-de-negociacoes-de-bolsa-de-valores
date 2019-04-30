2b00605b09a658c5bf52be8df438d6a9c9659ce7
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-04-30 14:10:00 -0300

    bug servidor (ajustando)

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -2,18 +2,18 @@
 
 class Negociacao {
 
-    constructor (_data, _quantidade, _valor) {
+    constructor(_data, _quantidade, _valor) {
 
-        Object.assign (this, { _quantidade, _valor });
-        this._data = new Date (data.getTime()),
-        Object.freeze (this); // Objeto imutável, não pode ser editado.
+        Object.assign(this, { _quantidade, _valor })
+        this._data = new Date(_data.getTime()),
+        Object.freeze(this); // Objeto imutável, não pode ser editado.
     }
 
     //  Métodos acessadores.
-    get data () { return new Date (this._data.getTime ()); }
-    get quantidade () { return this._quantidade; }
-    get valor () { return this._valor; }
-    get volume () { return this._quantidade * this._valor; }
+    get data() { return new Date (this._data.getTime ());}
+    get quantidade() { return this._quantidade;}
+    get valor() { return this._valor;}
+    get volume() { return this._quantidade * this._valor;}
 }
 
 
