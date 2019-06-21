e1a7ec6bc002afb35742ee0b3d659b7a04fb3f8d
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-06-21 17:42:52 -0300

    criação do objeto Date.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -3,8 +3,21 @@
 
 class NegociacaoController {
     
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+    
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+
+    }
+
     adiciona(event) {
+
         event.preventDefault();
-        alert("Chamei ação do Controlller.")
+
+        let data = new Date(this._inputData.value.replace(/-/g, ","));
+        console.log(data);
     }
 }
--- client/app/models/Negociacao.js
+++ client/app/models/Negociacao.js
@@ -4,11 +4,20 @@
 
 class Negociacao {
 
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor) {
+
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        Object.freeze(this);
     }
+
+    
+    get volume() { return this._quantidade * this._valor; }
+    get data() { return new Date(this._data.getTime()); }
+    get quantidade() { return this._quantidade; }
+    get valor() { return this._valor; }
 }
 
 
