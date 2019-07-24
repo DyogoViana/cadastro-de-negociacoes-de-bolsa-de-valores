4d41b581ee8d0a37a5f9c5f3c9b52e7352197429
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-07-24 14:55:02 -0300

    criado a instância de negociação, objeto Date, arrow function, spread operator (...), problema com a data resolvido.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -17,7 +17,31 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ","));
-        console.log(data);
+        //data
+        let data = new Date(...
+            this._inputData.value
+            .split("-")
+            .map((item, indice) => item - indice % 2) 
+        );
+        
+        //negociação
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
+
+
+
+
+
+
+// Dicas:
+
+// spread operator -- São as reticências antes do _this_. indica que ele desmenbrará o array e posiciona na mesma ordem no construtor.
+
+// item - indice % 2 -- ler item, menos indice, módulo dois.
--- client/app/models/Negociacao.js
+++ client/app/models/Negociacao.js
@@ -14,6 +14,7 @@ class Negociacao {
     }
 
     
+    // Métodos acessadores.
     get volume() { return this._quantidade * this._valor; }
     get data() { return new Date(this._data.getTime()); }
     get quantidade() { return this._quantidade; }
@@ -24,31 +25,7 @@ class Negociacao {
 
 
 
-
-// class Negociacao {
-
-//     constructor(_data, _quantidade, _valor) {
-
-//         Object.assign(this, { _quantidade, _valor })
-//         this._data = new Date(_data.getTime()),
-//         Object.freeze(this); // Objeto imutável, não pode ser editado.
-//     }
-
-//     //  Métodos acessadores.
-//     get data() { return new Date (this._data.getTime ());}
-//     get quantidade() { return this._quantidade;}
-//     get valor() { return this._valor;}
-//     get volume() { return this._quantidade * this._valor;}
-// }
-
-
-
-
-
-
-
-
-
+//Dicas:
 
 // o 'underline' indica ao programador que não pode ser alterada a propriedade.
 
