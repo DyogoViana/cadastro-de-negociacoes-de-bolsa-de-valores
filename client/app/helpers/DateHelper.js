7d97658ac6ef1b1d672dbf75c1e34ec7e0c0c0fd
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-08-29 14:33:53 -0300

    criação do método estático para a classe DateHelper.

--- client/app/helpers/DateHelper.js
+++ client/app/helpers/DateHelper.js
@@ -4,15 +4,23 @@
 // Classe para criação da data de negociação.
 class DateHelper {
 
+    constructor() {
+        throw new Error("Esta classe não pode ser instanciada.");
+    }
+
     // escreve a data num formato padrão ao usuário.
     // a data sai errada pq o Date guarda de 0 ~ 11.
-    dataParaTexto(data) {
+    static dataParaTexto(data) {
        return data.getDate()
             + "/" + (data.getMonth() + 1)
             + "/" + data.getFullYear();
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
         return new Date(...texto.split("-").map((item, indice) => item - indice % 2));
     }
 }
+
+
+// Anotações:
+// Agora com o static o 'DateHelper' pode ser chamado como uma class. Assim evita repetição.
