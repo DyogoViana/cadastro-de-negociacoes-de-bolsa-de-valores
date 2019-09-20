811e93e751945fe58f789b57b95ea8528ff728bc
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-09-20 14:14:39 -0300

    Template Strings usado na parte da validação da data (dd-mm-aaaa).

--- client/app/helpers/DateHelper.js
+++ client/app/helpers/DateHelper.js
@@ -11,12 +11,16 @@ class DateHelper {
     // escreve a data num formato padrão ao usuário.
     // a data sai errada pq o Date guarda de 0 ~ 11.
     static dataParaTexto(data) {
-       return data.getDate()
-            + "/" + (data.getMonth() + 1)
-            + "/" + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 
     static textoParaData(texto) {
+        // Expressão regular para validar a data no formato certo -- fail fast.
+        // A linha com o throw new só será executada se o if for falso, por isso, o sinal de !.
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
+            throw new Error("Deve está no formato aaaa-mm-dd.");
+        }
+
         return new Date(...texto.split("-").map((item, indice) => item - indice % 2));
     }
 }
