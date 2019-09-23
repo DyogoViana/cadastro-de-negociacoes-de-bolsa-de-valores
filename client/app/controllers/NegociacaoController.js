f19bc3eaa8a4224084231895522cbb51ff2c219c
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-09-23 20:49:08 -0300

    Criando o ListModel | add ListaNegociacoes.js; limpa o formulário; Criado dois métodos da 'controller' que não são chamados fora da classe, sinalizado pelo _ (underline).

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,22 +10,36 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
 
         event.preventDefault();
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao()); // Lista as negociações.
+        this._limpaFormulario();
 
-        //negociação
-        let negociacao = new Negociacao(
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    //cria uma negociação.
+    _criaNegociacao() {
+        return new Negociacao (
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    }
+
+    // Limpa o formulário e o foco vai para a data.
+    _limpaFormulario() {
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
 
---
+++ client/app/models/ListaNegociacoes.js
@@ -0,0 +1,33 @@
+// ListaNegociacoes.js
+
+
+
+
+// Cria lista de negociações.
+class ListaNegociacoes {
+
+    constructor() {
+        this._negociacoes = []
+    }
+
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+        return this._negociacoes;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//Dicas e anexos:
+// O prefixo _ (underline) para indicar que a lista não deve ser alterada.
