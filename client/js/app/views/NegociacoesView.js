00ec51b6950f9562f98c95e2fe415f71f107ebf5
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-10-02 21:20:01 -0300

    Totalizando o volume (falta melhorar o código, mas tá funcionando).

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -22,17 +22,31 @@ class NegociacoesView {
             </thead>
 
             <tbody>
-                ${model.negociacoes.map( n => { // 'n' = negociação.
-                    return `
-                    <tr>
-                        <td>${DateHelper.dataParaTexto(n.data)}</td>
-                        <td>${n.quantidade}</td>
-                        <td>${n.valor}</td>
-                        <td>${n.volume}</td>
-                    </tr>
-                        `
-                }).join("")}
+                ${model.negociacoes.map( n => // 'n' = negociação.
+                    `
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr>
+
+                    `).join("")}
             </tbody>
+
+            <tfoot>
+                // totalizar o volume.
+                <td colspan="3" style="text-align:right"><b>Total</b></td>
+                <td>
+                    ${
+                        (function() {
+                            let total = 0;
+                            model.negociacoes.forEach(n => total += n.volume);
+                            return total;
+                        })()
+                    }
+                </td>
+            </tfoot>
         </table>
         `;
     }
@@ -54,3 +68,7 @@ class NegociacoesView {
 // O 'innerHTML' será responsável por converter as strings em elementos do DOM. Isto será inserido com filho da <div>.
 
 // 'Join' = Concatena todos os dados do array e vira uma string.
+
+// Por ser uma arrow function, pode retirar o 'return' e as chaves. 
+
+// Immediately-invoked function expression - "IIFE" -  ou a função imediata.
