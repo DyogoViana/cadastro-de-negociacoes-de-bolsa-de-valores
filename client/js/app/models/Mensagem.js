47b4b625a24fd32ce5af72fb7475017f4a9abea5
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-10-23 16:02:20 -0300

    Criação do modelo - base -  de mensagem para dá um retorno ao usuário, quando adicionar uma negociação.

---
+++ client/js/app/models/Mensagem.js
@@ -0,0 +1,18 @@
+// Mensagem.js
+
+
+
+class Mensagem {
+
+    constructor(texto="olá!") {
+        this._texto = texto;
+    }
+    
+    get texto() {
+        return this._texto;
+    }
+
+    set texto(texto) {
+        this._texto = texto;
+    }
+}
