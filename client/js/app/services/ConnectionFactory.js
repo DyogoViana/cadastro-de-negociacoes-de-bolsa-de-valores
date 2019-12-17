07707f037af76480cf8183ca37e8e3179b103ae1
DyogoViana <dyogo.vianaa@gmail.com>
2019-12-16 21:40:31 -0300

    Criado 'stores' no IndexedDB.

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -2,8 +2,8 @@
 
 
 var store = ["negociacoes"];
-var dataBaseName = "Sistema de Cadastros da Bolsa de Valores";
-var version = 7;
+var dataBaseName = "Banco de Dados para Cadastros da Bolsa de Valores";
+var version = 8;
 
 class ConnectionFactory {
 
@@ -15,11 +15,31 @@ class ConnectionFactory {
         return new Promise((resolve, reject) => {
             let openRequest = window.indexedDB.open(dataBaseName, version);
 
-            openRequest.onupgradeneeded = event => {};
-
-            openRequest.onsuccess = event => {};
-
-            openRequest.onerror = event => {};
+            openRequest.onupgradeneeded = event => {
+                ConnectionFactory._createStores(event.target.result);
+            };
+            
+            openRequest.onsuccess = event => {
+                resolve(event.target.result);
+            };
+            
+            openRequest.onerror = event => {
+                console.log(event.target.error);
+                reject(event.target.error.name);
+            };
         });
+    }
+    
+    static _createStores(connection) {
+        store.forEach(store => {
+            if (connection.objectStoreNames.contains(store)) { // caso tenha uma store, ela será deletada.
+                connection.deleteObjectStore(store);
+            }
+    
+            connection.createObjectStore(store, { // Caso tudo esteja correto, será criado uma nova store.
+                autoIncrement: true
+            });
+        });
+
     }
 }
